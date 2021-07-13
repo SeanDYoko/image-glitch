8a8743fe08ac4c0be0b78c6a79b42ae568d4cdbf
SeanDYoko <sean@lobstermarketing.com>
2021-07-13 13:37:29 -0400

    added gatsby-ssr to resolve context for build

--- site/src/pages/author.js
+++ site/src/pages/author.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react"
+import React, { useContext } from "react"
 import { graphql } from "gatsby"
 import { stringify } from "qs"
 import { ImageElement } from "@kentico/gatsby-kontent-components"
@@ -6,7 +6,8 @@ import { ImageUrlBuilder } from "@kentico/kontent-delivery"
 import { AppContext } from "../context"
 
 const Author = ({ data }) => {
-  const { size, height } = useContext(AppContext)
+  const { size } = useContext(AppContext)
+
   if (!data.author.elements.avatar_image.value.length) {
     return <pre>Author avatar note set.</pre>
   }
@@ -30,78 +31,84 @@ const Author = ({ data }) => {
   const deliverySDKTransformedUrl = new ImageUrlBuilder(avatar.url)
     .withRectangleCrop(...rectSelection)
     .withWidth(imageQuery.w)
-  if(size < 768) {return (
-    <>
-      <header>
-        <h1>{data.author.elements.name.value}</h1>
-      </header>
-      <section>
-        <ImageElement
-          image={avatar}
-          width={800}
-          height={200}
-          backgroundColor="#bbbbbb"
-          alt={avatar.description}
-        />
-        <p>
-          This uses the <a href="https://github.com/Kentico/kontent-gatsby-packages/tree/master/packages/gatsby-kontent-components">ImageElement</a> component to display a
-          responsive image.
-        </p>
-      </section>
+  console.log(size)
+  if (size && size > 768) {
+    return (
+      <>
+        <header>
+          <h1>{data.author.elements.name.value}</h1>
+        </header>
+        <section>
+          <ImageElement
+            image={avatar}
+            width={800}
+            height={200}
+            backgroundColor="#bbbbbb"
+            alt={avatar.description}
+          />
+          <p>
+            This uses the
+            <a href="https://github.com/Kentico/kontent-gatsby-packages/tree/master/packages/gatsby-kontent-components">
+              ImageElement
+            </a>
+            component to display a responsive image.
+          </p>
+        </section>
 
-      <article
-        style={{ display: "flex", flexWrap: "wrap", alignItems: "flex-end" }}
-      >
-        <section style={{ padding: "1em", maxWidth: showcaseWidth }}>
-          <img
-            src={avatar.url}
-            alt={avatar.description || "Author avatar image"}
-            width="200px"
-          />
-          <p>
-            This is the basic loading of the asset in full size and setting its
-            width right in <tt>img</tt> tag (don't do that).
-          </p>
-        </section>
-        <section style={{ padding: "1em", maxWidth: showcaseWidth }}>
-          <img
-            src={`${avatar.url}?${stringify(imageQuery, { encode: false })}`}
-            alt={avatar.description || "Author avatar image"}
-          />
-          <p>
-            Showcase using{" "}
-            <a href="https://docs.kontent.ai/reference/image-transformation">
-              Kontent Image transformation API
-            </a>{" "}
-            and <a href="https://www.npmjs.com/package/qs">qs npm package.</a>{" "}
-            for query string construction.
-          </p>
-        </section>
-      </article>
+        <article
+          style={{ display: "flex", flexWrap: "wrap", alignItems: "flex-end" }}
+        >
+          <section style={{ padding: "1em", maxWidth: showcaseWidth }}>
+            <img
+              src={avatar.url}
+              alt={avatar.description || "Author avatar image"}
+              width="200px"
+            />
+            <p>
+              This is the basic loading of the asset in full size and setting
+              its width right in <tt>img</tt> tag (don't do that).
+            </p>
+          </section>
+          <section style={{ padding: "1em", maxWidth: showcaseWidth }}>
+            <img
+              src={`${avatar.url}?${stringify(imageQuery, { encode: false })}`}
+              alt={avatar.description || "Author avatar image"}
+            />
+            <p>
+              Showcase using{" "}
+              <a href="https://docs.kontent.ai/reference/image-transformation">
+                Kontent Image transformation API
+              </a>{" "}
+              and <a href="https://www.npmjs.com/package/qs">qs npm package.</a>{" "}
+              for query string construction.
+            </p>
+          </section>
+        </article>
 
-      <article
-        style={{ display: "flex", flexWrap: "wrap", alignItems: "flex-end" }}
-      >
-        <section style={{ padding: "1em", maxWidth: showcaseWidth }}>
-          <img
-            src={deliverySDKTransformedUrl.getUrl()}
-            alt={avatar.description || "Author avatar image"}
-          />
-          <p>
-            Showcase using{" "}
-            <a href="https://docs.kontent.ai/reference/image-transformation">
-              Kontent Image transformation API
-            </a>{" "}
-            and{" "}
-            <a href="https://github.com/Kentico/kontent-delivery-sdk-js/blob/master/DOCS.md#image-transformations">
-              Delivery SDK
-            </a>{" "}
-            for query string construction.
-          </p>
-        </section>
-      </article>
-    </>
-  )} else return <>hello world </>
+        <article
+          style={{ display: "flex", flexWrap: "wrap", alignItems: "flex-end" }}
+        >
+          <section style={{ padding: "1em", maxWidth: showcaseWidth }}>
+            <img
+              src={deliverySDKTransformedUrl.getUrl()}
+              alt={avatar.description || "Author avatar image"}
+            />
+            <p>
+              Showcase using{" "}
+              <a href="https://docs.kontent.ai/reference/image-transformation">
+                Kontent Image transformation API
+              </a>{" "}
+              and{" "}
+              <a href="https://github.com/Kentico/kontent-delivery-sdk-js/blob/master/DOCS.md#image-transformations">
+                Delivery SDK
+              </a>{" "}
+              for query string construction.
+            </p>
+          </section>
+        </article>
+      </>
+    )
+  } else return <>hello world </>
 }
 
 export const query = graphql`
